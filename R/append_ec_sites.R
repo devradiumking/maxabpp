1b05fd565faad0a133a7f9325d718fcafd1d3965
devradiumking <devradiumking@hotmail.com>
2020-05-08 19:02:42 -0400

    Initial

---
+++ R/append_ec_sites.R
@@ -0,0 +1,136 @@
+#Main
+append_ec_sites <- function(LFQ_table, quantitation_level) {
+
+library(dplyr)
+library(tidyr)
+library(stringdist)
+library(rscripting)
+library(stringr)
+library(tidyverse)
+library(ggplot2)
+library(UniprotR)
+proteome_database <-read.delim("homo_sapiens.txt", header=TRUE, sep="\t")
+
+#Internal function 1: extract an ec number for the major protein in a protein group from the database
+database_search_ec <- function (protein_group) {
+  protein_hits <- vector()
+  ec <- vector()
+  major_protein <- str_split(protein_group[1], ";")[[1]][1]
+  index <- grep(major_protein, proteome_database$Entry)
+  ec <- as.character(proteome_database$EC.number[index])
+  if (length(ec) > 0) {
+  return(ec)
+  } else {return ("na")}
+}
+
+#Internal function 2: translate ec number to the class name and attach 
+attach_enzyme_class <- function (LFQ_table) {
+  len <- nrow(LFQ_table)
+  ec_list <- vector()
+  ec <- NA
+  for (i in 1:len) {
+    ec <- database_search_ec(LFQ_table$Proteins[i])
+    ec <- str_trunc(ec, 1, "right", ellipsis = "")
+    switch(ec,
+           "1" = {ec_list[i] <- "Oxidoreductase"},
+           "2" = {ec_list[i] <- "Transferase"},
+           "3" = {ec_list[i] <- "Hydrolase"},
+           "4" = {ec_list[i] <- "Lyase"},
+           "5" = {ec_list[i] <- "Isomerase"},
+           "6" = {ec_list[i] <- "Ligase"},
+           "7" = {ec_list[i] <- "Translocase"},
+           ec_list[i] <- "Others"
+    )
+  }
+  return(cbind(LFQ_table, ec_list))
+}
+
+#Internal function 3: extract numeric site infomation
+extract_sites <- function (column_of_sites, prefix) {
+  test <- str_trim(str_split(column_of_sites, ";")[[1]])
+  criteria_test <- test %in% grep(prefix, test, value = TRUE)
+  return (parse_number(subset(test, criteria_test)))
+}
+
+#Internal function 4: extract a protein sequence for each protein in a protein group from the database
+database_search_sites <- function (protein_group) {
+  protein_hits <- vector()
+  binding_sites <- vector()
+  active_sites <- vector()
+  other_sites <- vector()
+  for (n in 1:length(protein_group)) {
+    index <- grep(protein_group[n], proteome_database$Entry)
+    protein_hits[n] <- as.character(proteome_database$Sequence[index])
+    s1 <- extract_sites(proteome_database$Binding.site[index], "BINDING")
+    if (length(s1) > 0) {
+      binding_sites[n] <- s1
+    } else {
+      binding_sites[n] <- "no"
+    }
+    s2 <- extract_sites(proteome_database$Active.site[index], "ACT_SITE")
+    if (length(s2) > 0) {
+      active_sites[n] <- s2
+    } else {
+      active_sites[n] <- "no"
+    }
+    s3 <- extract_sites(proteome_database$Site[index], "SITE")
+    if (length(s3) > 0) {
+      other_sites[n] <- s3
+    } else {
+      other_sites[n] <- "no"
+    }
+  }
+  return(cbind(protein_hits, binding_sites, active_sites, other_sites))
+}
+
+#Internal function 5: compare reported sites to tryptic peptides, get site coverage of each tryptic peptide
+compare_sites <- function (site_positions, n_term, c_term) {
+  num_sites <- length(site_positions)
+  site <- NULL
+  output <- NULL
+  for (i in 1:num_sites) {
+    site <- as.integer(site_positions[i])
+    if (is.na(site) | is.na(n_term) | is.na(c_term) | n_term == c_term){
+      output <- paste0(output, ";", "na")
+    } else if (n_term <= site && c_term >= site) {
+      output <- paste0(output, ";", site)
+    } else {
+      output <- paste0(output, ";", "na")
+    }
+  }
+  return(str_sub(output, 2))
+}
+
+#Internal function 6: append site information
+match_mod_peptides <- function (LFQ_table) {
+  output <- data.frame(binding_sites = NULL, active_sites = NULL, other_sites = NULL)
+  for (i in 1:nrow(LFQ_table)) {
+    proteins_list <- LFQ_table$Proteins
+    peptides_list <- LFQ_table$parent_sequence
+    binding_sites <- data.frame()
+    active_sites <- data.frame()
+    other_sites <- data.frame()
+    all_sites <- data.frame()
+    empty_site_table <- data.frame(protein_hits = "", binding_sites = "", active_sites = "", other_sites = "")
+    sites_table <- tryCatch(as.data.frame(database_search_sites(str_split(proteins_list[i], ";")[[1]])), error = function(c) empty_site_table)
+    protein_sequence <- as.vector(sites_table[1,1])
+    peptide_sequence <- peptides_list[i]
+    mod_peptide_locations <- str_locate(protein_sequence, peptide_sequence)
+    start <- as.integer(mod_peptide_locations[1])
+    end <- as.integer(mod_peptide_locations[2])
+    binding_site_positions <- as.integer(paste(sites_table[,2]))
+    active_site_positions <- as.integer(paste(sites_table[,3]))
+    other_site_positions <- as.integer(paste(sites_table[,4]))
+    binding_sites <- compare_sites(binding_site_positions, start, end)
+    active_sites <- compare_sites(active_site_positions, start, end)
+    other_sites <- compare_sites(other_site_positions, start, end)
+    all_sites <- cbind(binding_sites, active_sites, other_sites)
+    output <- rbind(output, all_sites)
+  }
+  return(cbind(LFQ_table, output))
+}
+
+if (quantitation_level == "peptide") {
+return(cbind(attach_enzyme_class(match_mod_peptides(LFQ_table))))
+} else {return(attach_enzyme_class(LFQ_table))}
+}
---
+++ R/pairwise_LFQ_fucntion.R
@@ -0,0 +1,236 @@
+
+pairwise_LFQ <- function (raw = read.delim("modificationSpecificPeptides.txt", header=TRUE, sep="\t"), metadata = read.delim("metadata.txt", header=TRUE, sep="\t"), 
+name_probe_mod, max_each_mod = 1, max_total_mods = 1, quantitation_level = "peptide" , background_check = FALSE) {
+
+library(dplyr)
+library(tidyr)
+library(stringdist)
+library(rscripting)
+library(stringr)
+library(tidyverse)
+  
+#* Establish a new 'ArgCheck' object
+Check <- ArgumentCheck::newArgCheck()
+num_mods <- length(name_probe_mod)
+#* Add an error if raw or metadata is missing
+if (is.null(raw))
+  ArgumentCheck::addError(
+    msg = "'raw' is missing",
+    argcheck = Check
+  )
+if (is.null(metadata))
+  ArgumentCheck::addError(
+    msg = "'metadata' is missing",
+    argcheck = Check
+  )
+#* Add an error if number of input mods is less than 1
+if (num_mods < 1 || num_mods > 10 || is.character(name_probe_mod) == FALSE) 
+  ArgumentCheck::addError(
+    msg = "'name_probe_mod' must be a vector containing 1 to 10 string components",
+    argcheck = Check
+  )
+
+#* Return errors and warnings (if any)
+ArgumentCheck::finishArgCheck(Check)
+
+
+#Internal function 1: appending a column named "parent sequence" of the longest miscleaved version of each peptide sequence
+#If one cleaved peptide can map to multiple miscleaved peptides, the "parent sequence" will be a concatenated string of all
+add_parent_sequence <- function (dataset) {
+  seq <- dataset$Sequence
+  for (i in 1:length(seq)) {
+    related_seq <- unique(as.character(subset(seq, grepl(seq[i],seq))))
+    length_seq <- nchar(related_seq)
+    num_seq <- length(length_seq)
+    if (num_seq == 1) {
+      dataset$parent_sequence[i] <- as.character(related_seq)
+    } else if (num_seq == 2) {
+      dataset$parent_sequence[i] <- as.character(related_seq[2])
+    } else {
+      parents <- subset(related_seq, nchar(related_seq) == max(length_seq))
+      if (length(parents) == 1) {
+        dataset$parent_sequence[i] <- parents
+      } else {
+        dataset$parent_sequence[i] <- paste(parents, collapse = ";")
+      }
+    }
+  }
+  return(dataset)
+}
+
+#Internal function 2: replacing column names with metadata replicate groups
+replace_column_names <- function (interim_data, metadata) {
+  num_column_rename <- length(colnames(interim_data %>%  select(starts_with("Intensity."))))
+  if (nrow(metadata) == num_column_rename) {
+    for (k in 1:num_column_rename) {
+      names(interim_data)[names(interim_data) == metadata[,1][k]] <- metadata[,2][k]
+    }
+    return(interim_data)
+  } else {
+    return(NULL)
+  }
+}
+
+#Internal function 3: replace characters in a data frame
+replace_val <- function (data, unwanted_value, replacement) {
+  data <- apply(data,2,function(x)gsub(unwanted_value, replacement,x))
+return(data)
+}
+
+#Internal function 4A: determine if two groups of measurements have the same variance using F-test
+if_equal_variance <- function (group1, group2) {
+  if (mean(group1) == 0 && mean(group2) == 0) {
+    return(TRUE)
+  } else {
+    F_result <- var.test(group1, group2, ratio = 1,
+                         alternative = c("two.sided"),
+                         conf.level = 0.95)
+    if (F_result$p.value <= 0.05) {
+      return(FALSE)
+    } else {
+      return(TRUE)
+    }
+  }
+}
+  
+#Internal function 4B: generate a table for volcano plot
+generate_volcano_table <- function (interim_data) {
+  result <- data.frame()
+  result_p <- data.frame()
+  result_fc <- data.frame()
+  interim_data_select <- subset(interim_data, select = -c(2,3,4,5))
+  for (j in 1:nrow(interim_data_select)) {
+    entry_data <- as.data.frame(t(interim_data_select[j,]))
+    entry_data <- rownames_to_column(entry_data, "Group")
+    entry_data <- separate(entry_data, Group, c("remain","trim"), sep = "\\.")
+    for (k in 1:length(filtered_pairs)) {
+      #Define group2 as the inhibitor-treated at a higher inhibitor concentration
+      entry_data_group1 <- as.numeric(as.vector(subset(entry_data, remain == filtered_pairs$V1[k], stringasfactor = FALSE)[,3]))
+      entry_data_group2 <- as.numeric(as.vector(subset(entry_data, remain == filtered_pairs$V2[k], stringasfactor = FALSE)[,3]))
+      #Under the presumption that the higher-concentration inhibitor-treated group2 should have a lower mean intensity
+      #Null hypothesis: group1 and group2 have the same intensity
+      #Alternative hypothesis, group1 has greater intensity
+      t_result <- NULL
+       t_result <- t.test(entry_data_group1, y = entry_data_group2,
+                        alternative = c("greater"),
+                        #Call function 4A to determine variance equality
+                        mu = 0, paired = FALSE, var.equal = if_equal_variance(entry_data_group1, entry_data_group2),
+                        conf.level = 0.95)
+      p_value <- t_result$p.value
+      mean1 <- as.numeric(t_result$estimate[1])
+      mean2 <- as.numeric(t_result$estimate[2])
+      fold_change <- mean2/mean1
+      result_p[j,k] <- -log10(p_value)
+      result_fc[j,k] <- log2(fold_change)
+      colnames(result_p)[k] <- paste(filtered_pairs$V2[k],"_vs_", filtered_pairs$V1[k],"_-log10p-value")
+      colnames(result_fc)[k] <- paste(filtered_pairs$V2[k],"_vs_", filtered_pairs$V1[k],"_log2fold_change")
+    }
+    result <- cbind(result_p, result_fc)
+  }
+  if (colnames(interim_data)[1] == "parent_sequence") {
+    result <- cbind(interim_data[,1:6], result)
+  } else {
+    result <- cbind(interim_data[,1:5], result)
+  }
+  return (result)
+}
+
+#Internal function 5A: Recursively calculate the total mods
+criteria_total <- function (dataframe, probe_mods) {
+  type_mods <- length(probe_mods)
+  m <- dataframe[[probe_mods[type_mods]]]
+  if (type_mods == 1) {
+    return(m)
+  } else {
+    probe_mods <- head(probe_mods, type_mods - 1)
+    return(m + criteria_total(dataframe, probe_mods))
+  }
+}
+
+#Internal function 5B: Filter dataset according to the numbers of max mods
+subset_mod <- function (dataframe, probe_mods, max_each_mod, max_total_mods) {
+  type_mods <- length(probe_mods)
+  criteria_total <- criteria_total(dataframe, probe_mods)
+  subset1 <- dataframe[criteria_total <= max_total_mods , ]
+  for (i in 1:type_mods) {
+    criteria_single <- subset1[[probe_mods[i]]]
+    subset1 <- subset1[criteria_single <= max_each_mod , ]
+  }
+  return(subset1)
+}
+
+#Replace space and dash characters with dots due to the limitation of column renaming in R
+metadata <- replace_val(metadata,'\\s+', '.')
+metadata <- replace_val(metadata,'\\-', '.')
+sample_groups <- unique(metadata[,2])
+###Conditional statement for the quantititaion of non-probe-modified peptides
+if (background_check == TRUE) {
+  probe_mods <- gsub("-", ".", gsub(" ", ".", gsub("[()]", ".", name_probe_mod)))
+  filtered_step1 <- subset(filter(raw, !mgrepl(probe_mods, Modifications, op ="|")), Reverse !="+")
+  column_names <- colnames(raw)
+  filtered_step2 <- add_parent_sequence(filtered_step1)
+  mod_validation <- TRUE
+} else {
+###Conditional statement probe modification-specific quantitation  
+ #Replace special characters with dots within the probe modification string due to the limitation of column renaming in R
+ probe_mods <- gsub("-", ".", gsub(" ", ".", gsub("[()]", ".", name_probe_mod)))
+ #Extract a subset containing non-reverse peptides carrying probe modifications
+ filtered_step1 <- subset(filter(raw, mgrepl(probe_mods, Modifications, op ="|")), Reverse !="+")
+ #Validate user defined probe modification
+ column_names <- colnames(raw)
+ mod_col_index <- match(probe_mods,column_names,nomatch = 0)
+ mod_validation <- mod_col_index > 0
+ #Filter out peptides carrying more or less than specified numbers of probe modifications on a single peptide
+ filtered_step2 <- subset_mod(filtered_step1, probe_mods, max_each_mod, max_total_mods)
+ #Call function 2
+ filtered_step2 <- add_parent_sequence(filtered_step2)
+}
+#Compare string similarity of sample groups and generate t.test pairs automatically
+pairs <- t(combn(sample_groups,2))
+V3 <- stringsim(pairs[,1],pairs[,2])
+pairs <- cbind(pairs,V3)
+filtered_pairs <- filter(as.data.frame(pairs,stringsAsFactors = FALSE), V3 > 0.8)
+filtered_pairs <- filter(filtered_pairs, V3 < 1)
+#Swap values so column V1 contains only shorter values
+b <- str_length(filtered_pairs$V1) < str_length(filtered_pairs$V2)
+for (i in 1:length(b)) {
+  longer_value <- filtered_pairs$V1[i]
+  shorter_value <- filtered_pairs$V2[i]
+  if (b[i] == FALSE) {
+    filtered_pairs$V1[i] <- shorter_value
+    filtered_pairs$V2[i] <- longer_value
+  }
+}
+if (quantitation_level == "peptide" && mod_validation == TRUE) {
+  ##Quantitation at peptide level
+  #Compress/summarize intensity values of each peptide for each sample, extract columns containing sequence information and intensity data
+  by_sequence <- filtered_step2 %>%  select(parent_sequence, Sequence, Modifications, Proteins, Gene.Names, Protein.Names, starts_with("Intensity.")) %>% group_by(parent_sequence)
+  intensity_subset1 <- by_sequence %>% summarise_if(is.numeric, sum)
+  #Collapse name strings with ; as the delimiter
+  name_subset1 <- by_sequence %>% summarise_at(c("Sequence","Modifications", "Proteins", "Gene.Names", "Protein.Names"), ~paste(.x, collapse="; "))
+  #Combine two parts as a new dataset
+  interim_peptide_data <- cbind(name_subset1, subset(intensity_subset1, select = -parent_sequence))
+  #Call function 2
+  interim_peptide_data <- replace_column_names(interim_peptide_data, metadata)
+  #Generate peptide level output
+  peptide_volcano_table <- generate_volcano_table(interim_peptide_data)
+  return (peptide_volcano_table)
+ } else if (quantitation_level == "protein" && mod_validation == TRUE) {
+   ##Quantitation at protein level
+   #Compress/summarize intensity values of each protein/protein group for each sample, extract columns containing sequence information and intensity data
+   by_proteins <- filtered_step2 %>%  select(Sequence, Modifications, Proteins, Gene.Names, Protein.Names, starts_with("Intensity.")) %>% group_by(Proteins)
+   intensity_subset2 <- by_proteins %>% summarise_if(is.numeric, sum)
+   #Collapse name strings with ; as the delimiter
+   name_subset2 <- by_proteins %>% summarise_at(c("Modifications", "Sequence", "Gene.Names", "Protein.Names"), ~paste(.x, collapse="; "))
+   #Combine two parts as a new dataset
+   interim_protein_data <- cbind(name_subset2, subset(intensity_subset2, select = -Proteins))
+   #Call function 2
+   interim_protein_data <- replace_column_names(interim_protein_data, metadata)
+   #Generate protein level output
+   protein_volcano_table <- generate_volcano_table(interim_protein_data)
+  return (protein_volcano_table)
+ } else {
+   return(NULL)
+ }
+}
+
