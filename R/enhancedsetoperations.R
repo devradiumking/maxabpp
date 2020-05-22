656d2b4b25a33f41289bc0cc8c4c0799f9c4cc88
devradiumking <devradiumking@hotmail.com>
2020-05-22 02:39:24 -0400

    Add mgrepl

--- R/enhancedsetoperations.R
+++ R/enhancedsetoperations.R
@@ -36,3 +36,15 @@ quick_venn_table=function(x){
   Res=data.frame(Entry=allE,Venn.area=venn_areas,stringsAsFactors=FALSE)
  return(Res)
 }
+#' Perform grepl on multiple patterns; it's like  AND-ing or OR-ing successive
+#'  grepl statements.
+#'
+#' @export
+#' @param pattern character vector of patterns
+#' @param x the character vector to search
+#' @param op logical vector operator back quoted, defaults to `|`
+#' @param ... further arguments for \code{grepl} like \code{fixed} etc.
+#' @return logical vector
+mgrepl <- function(pattern, x, op = `|`, ... ){
+  Reduce(op, lapply(pattern, grepl, x, ...))
+}
